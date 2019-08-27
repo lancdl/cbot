c848a59608ce28a321f17ff5608fae335ec30e2a
lanc <lanccllc1@gmail.com>
2019-08-28 01:01:04 +0300

    Update config_defaults.js

--- config_defaults.js
+++ config_defaults.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: 'NTk4MDk2ODM0NTE0MjU1ODcy.XTH6oA._ErJ87ExwkfIbtiEtfMYIG_yVyE',
+    token: 'TOKEN',
     vnum: '0.5.0',
     v: 'Callback v0.5.0 by lanc | 23 aug 2019',
     bot_id: '598096834514255872',
