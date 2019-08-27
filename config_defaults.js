8a2995d227e566429ec63fe469f79c8ebf489e16
lanc <lanccllc1@gmail.com>
2019-08-27 21:30:34 +0300

    Create config_defaults.js

---
+++ config_defaults.js
@@ -0,0 +1,7 @@
+module.exports = {
+    token: 'NTk4MDk2ODM0NTE0MjU1ODcy.XTH6oA._ErJ87ExwkfIbtiEtfMYIG_yVyE',
+    vnum: '0.5.0',
+    v: 'Callback v0.5.0 by lanc | 23 aug 2019',
+    bot_id: '598096834514255872',
+    dev_id: '235750839698980864'
+}
